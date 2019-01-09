dcfd503e335c9fbff41b313085437d196760ad56
fazerog02 <fazerog02@gmail.com>
2019-01-09 20:25:33 +0900

    [updata]createBlog

--- js/createBlog.js
+++ js/createBlog.js
@@ -1,8 +1,40 @@
 let realTimePreview = setInterval(preview, 1000);
 let previewArea = document.getElementById("previewArea");
+
+let inputText = document.getElementById("editBlog");
 function preview(){
-    let text = document.getElementById("editBlog");
-    previewArea.innerHTML = text.value;
+    if(document.getElementById("isRealTime").checked){
+        let inputText = document.getElementById("editBlog");
+        let text =
+            "<div class='header-pre'>\n" +
+            "    <h1><a href='/'>FazRoom</a></h1>\n" +
+            "    <div id='header-menu-pre'>\n" +
+            "        <a href='/about' style='text-decoration: none;'>\n" +
+            "            <div class='header-box-about-pre'>\n" +
+            "                <div class='header-box-text-pre'>ABOUT</div>\n" +
+            "            </div>\n" +
+            "        </a>\n" +
+            "        <a href='/link' style='text-decoration: none;'>\n" +
+            "            <div class='header-box-link-pre'>\n" +
+            "                <div class='header-box-text-pre'>LINK</div>\n" +
+            "            </div>\n" +
+            "        </a>\n" +
+            "        <a href='/projects' style='text-decoration: none;'>\n" +
+            "            <div class='header-box-projects-pre'>\n" +
+            "                <div class='header-box-text-pre'>PROJECTS</div>\n" +
+            "            </div>\n" +
+            "        </a>\n" +
+            "    </div>\n" +
+            "</div>\n" +
+            "<div id='header-sideNav-pre' class='close'></div>\n" +
+            "<div id='content-pre'>\n" +
+            inputText.value +
+            "</div>\n" +
+            "<div class='footer-pre'>\n" +
+            "    <a href='about'>©2019 fazerog</a>\n" +
+            "</div>";
+        previewArea.innerHTML = text;
+    }
 }
 
 function downloadHtml(){
